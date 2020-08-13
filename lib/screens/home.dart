4c3d6debb62efdf0f97875830e07cb1f8cfd449d
Filip Ljubic <f.ljubic7@hotmail.com>
2020-08-13 17:25:54 +0200

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// TODO: - fix search bar
+///       - fix draggablescrollview
+///       - make it beautiful
+///       - add animations
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
