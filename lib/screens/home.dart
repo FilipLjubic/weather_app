80de5bc5baeaa795ade0b989d692d87ed7c566b6
FilipLjubic <filip.ljubic@fer.hr>
2020-09-01 22:24:12 +0200

    changed search app bar and draggable scrollable sheet to look better

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,85 +10,80 @@ import 'future_days.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      resizeToAvoidBottomInset: false,
-      appBar: FloatAppBar(),
-      body: Stack(
-        children: [
-          // image container
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/zagreb.jpg"),
-                fit: BoxFit.cover,
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            // image container
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/zagreb.jpg"),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          DraggableScrollableSheet(
-            maxChildSize: 0.7,
-            minChildSize: 0.2,
-            initialChildSize: 0.3,
-            builder: (context, scrollController) => Container(
-              child: ListView.builder(
-                controller: scrollController,
-                itemCount: 5,
-                itemBuilder: (context, index) => ListTile(
-                  title: GestureDetector(
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (_) => FutureDays()),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+                child: Card(
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  color: Color(0x99D2F6FF).withOpacity(0.55),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.location_on,
+                      color: Colors.black45,
                     ),
-                    child: Text("Item $index"),
+                    title: TextField(
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.go,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        // set to current location
+                        hintText: "Enter a location",
+                      ),
+                    ),
                   ),
                 ),
               ),
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
+            ),
+            DraggableScrollableSheet(
+              maxChildSize: 0.5,
+              minChildSize: 0.2,
+              initialChildSize: 0.3,
+              builder: (context, scrollController) => Container(
+                child: ListView.builder(
+                  controller: scrollController,
+                  itemCount: 5,
+                  itemBuilder: (context, index) => ListTile(
+                    title: GestureDetector(
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => FutureDays()),
+                      ),
+                      child: Text("Item $index"),
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FloatAppBar extends StatelessWidget with PreferredSizeWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20.0),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0),
-          ),
-          color: Color(0x99D2F6FF),
-          child: ListTile(
-            leading: Icon(
-              Icons.location_on,
-              color: Colors.grey,
-            ),
-            title: TextField(
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.go,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Search...",
-              ),
-            ),
-          ),
+          ],
         ),
       ),
     );
   }
-
-  @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
